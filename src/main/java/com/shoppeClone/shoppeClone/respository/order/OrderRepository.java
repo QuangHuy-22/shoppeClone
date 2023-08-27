5683c5dc8dc0628206570ca76ed844377705e748
vanthinhnh <thinhnguyen2152@gmail.com>
2023-08-27 22:22:44 +0700

    update delete order

--- src/main/java/com/shoppeClone/shoppeClone/respository/order/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shoppeClone.shoppeClone.respository.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shoppeClone.shoppeClone.entity.OrderEntity;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long>{
-
-}
--- src/main/java/com/shoppeClone/shoppeClone/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.shoppeClone.shoppeClone.service;
-
-import com.shoppeClone.shoppeClone.dto.order.CreateOrderDTO;
-import com.shoppeClone.shoppeClone.dto.order.OrderDTO;
-
-public interface OrderService {
-
-	OrderDTO createOrder(CreateOrderDTO createOrderDTO);
-}
